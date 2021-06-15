ee0696d5c071d9ab64b3d93df863813484829cef
Luiz Fernando Arêas <lfernandoareas@gmail.com>
2021-06-15 06:26:14 -0300

    implemt do RestTemplate

--- rh-folha/src/main/java/com/tehilim/rhfolha/services/PagamentoService.java
+++ rh-folha/src/main/java/com/tehilim/rhfolha/services/PagamentoService.java
@@ -1,14 +1,31 @@
 package com.tehilim.rhfolha.services;
 
-import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.tehilim.rhfolha.entities.Func;
 import com.tehilim.rhfolha.entities.Pagamento;
 
 @Service
 public class PagamentoService {
 	
+	@Value("${rh-func.host}")
+	private String funcHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Pagamento getPagamento(Long funcId, Integer dias) {
-		return new Pagamento("Luiz", 250.0, dias);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", "" + funcId);
+		
+		Func func = restTemplate.getForObject(funcHost + "/{id}", Func.class, uriVariables);
+		return new Pagamento(func.getNome(), func.getSalarioDia(), dias);
 		
 	}
 }
